4ba20733d4ac09b039e9a8bb556261d942dda244
zhouhao15 <zhouhao15@baidu.com>
2021-01-15 17:38:13 +0800

    feature: 导出方式调整

--- src/packages/robo-error-page/index.js
+++ src/packages/robo-error-page/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by zh on 2021/1/15.
  */
-import RoboErrorPage from 'index.vue';
+import RoboErrorPage from './index.vue';
 
 RoboErrorPage.install = function(Vue) {
     Vue.component(RoboErrorPage.name, RoboErrorPage);
