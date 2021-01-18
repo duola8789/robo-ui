1519a0c4d2210f3b5eb676fecbcd6c8c023b9776
zhouhao15 <zhouhao15@baidu.com>
2021-01-18 12:07:49 +0800

    feature: 导出时使用相对路径

--- src/packages/robo-font-icon/index.js
+++ src/packages/robo-font-icon/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by zh on 2021/1/15.
  */
-import RoboFontIcon from 'index.vue';
+import RoboFontIcon from './index.vue';
 
 RoboFontIcon.install = function(Vue) {
     Vue.component(RoboFontIcon.name, RoboFontIcon);
