0e69bd54a6e244b2dbf8ace36b17cc95d881a3a6
zhouhao15 <zhouhao15@baidu.com>
2021-01-18 09:01:47 +0800

    feature: Types更新

---
+++ types/packages/select-all.ts
@@ -0,0 +1,3 @@
+import {RoboUIComponent} from '../component';
+
+export declare class RoboSelectAll extends RoboUIComponent {}
--- types/robo-ui.d.ts
+++ types/robo-ui.d.ts
@@ -1,11 +1,19 @@
+import Vue from 'vue';
+
 import {RoboErrorPage} from './packages/error-page';
 import {RoboFontIcon} from './packages/font-icon';
 import {RoboOverflowText} from './packages/overflow-text';
 import {RoboAsideNavMenu} from './packages/aside-nav-menu';
 import {RoboAsideNav} from './packages/aside-nav';
+import {RoboSelectAll} from './packages/select-all';
+import {RoboSelectMultiCheckbox} from './packages/select-multi-checkbox';
+
+export function install(vue: typeof Vue): void;
 
 export class ErrorPage extends RoboErrorPage {}
 export class FontIcon extends RoboFontIcon {}
 export class OverflowText extends RoboOverflowText {}
 export class AsideNavMenu extends RoboAsideNavMenu {}
 export class AsideNav extends RoboAsideNav {}
+export class SelectAll extends RoboSelectAll {}
+export class SelectMultiCheckbox extends RoboSelectMultiCheckbox {}
