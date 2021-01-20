40ff754bc1f64a4ec54ef237faad29ee4b70c1ad
zhouhao15 <zhouhao15@baidu.com>
2021-01-20 11:21:10 +0800

    feature: robo-icon 的默认图标功能不完善，当出现在 dialog 中时会影响正常工作，先去掉

--- src/packages/robo-font-icon/index.vue
+++ src/packages/robo-font-icon/index.vue
@@ -1,33 +1,17 @@
 <template>
-    <svg ref="iconSvg" class="robo-font-icon" aria-hidden="true">
-        <use ref="iconFont" :xlink:href="iconHref"></use>
+    <svg class="robo-font-icon" aria-hidden="true">
+        <use :xlink:href="iconHref"></use>
     </svg>
 </template>
 
 <script lang="ts">
-import {Component, Vue, Prop, Watch, Ref} from 'vue-property-decorator';
-
-const DEFAULT_ICON: string = 'icon-sentiment-very-dissa';
+import {Component, Vue, Prop} from 'vue-property-decorator';
 
 @Component
 export default class RoboFontIcon extends Vue {
     @Prop({required: true, type: String}) href!: string;
 
-    @Ref() iconSvg!: HTMLElement;
-    @Ref() iconFont!: HTMLElement;
-
-    iconHref = `#${DEFAULT_ICON}`;
-
-    // 目前如果无法根据元素可见性（display）变化（比如出现在 dialog 中）时，设置默认图标
-    @Watch('href', {immediate: true})
-    hrefWatch() {
-        this.iconHref = `#${this.href || DEFAULT_ICON}`;
-        setTimeout(() => {
-            if (this.iconSvg.getBoundingClientRect().width !== 0 && this.iconFont.getBoundingClientRect().width === 0) {
-                this.iconHref = `#${DEFAULT_ICON}`;
-            }
-        });
-    }
+    iconHref = `#${this.href}`;
 }
 </script>
 
