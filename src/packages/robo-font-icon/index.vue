c35d6d3d79ea9cc04480ee92816e6d45a05360eb
zhouhao15 <zhouhao15@baidu.com>
2021-01-20 11:19:52 +0800

    feature: robo-icon 优化

--- src/packages/robo-font-icon/index.vue
+++ src/packages/robo-font-icon/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg class="robo-font-icon" aria-hidden="true">
+    <svg ref="iconSvg" class="robo-font-icon" aria-hidden="true">
         <use ref="iconFont" :xlink:href="iconHref"></use>
     </svg>
 </template>
@@ -13,15 +13,17 @@ const DEFAULT_ICON: string = 'icon-sentiment-very-dissa';
 export default class RoboFontIcon extends Vue {
     @Prop({required: true, type: String}) href!: string;
 
+    @Ref() iconSvg!: HTMLElement;
     @Ref() iconFont!: HTMLElement;
 
     iconHref = `#${DEFAULT_ICON}`;
 
+    // 目前如果无法根据元素可见性（display）变化（比如出现在 dialog 中）时，设置默认图标
     @Watch('href', {immediate: true})
     hrefWatch() {
         this.iconHref = `#${this.href || DEFAULT_ICON}`;
         setTimeout(() => {
-            if (this.iconFont.getBoundingClientRect().width === 0) {
+            if (this.iconSvg.getBoundingClientRect().width !== 0 && this.iconFont.getBoundingClientRect().width === 0) {
                 this.iconHref = `#${DEFAULT_ICON}`;
             }
         });
