6bb0647ff314d401616187474a70f6c6058219c4
zhouhao15 <zhouhao15@baidu.com>
2021-01-27 09:15:10 +0800

    feature: RoboSelectMultiGroup组件更新使用说明

--- src/views/robo-ui/pages/select-multi-group.vue
+++ src/views/robo-ui/pages/select-multi-group.vue
@@ -1,26 +1,30 @@
 <template>
     <div class="page-container">
-        <el-card header="使用缓存 - 传入一个由 cacheKey 组成的数组">
+        <el-card
+            header="使用缓存 - 传入一个由 cacheKey 组成的数组，cache-key 应该提前在插件中配置好，另外传入的 value 中必须包含 cacheKey 中的字段"
+        >
             <robo-select-multi-group title="又吃又玩" :cache-config="cacheConfig1" :value.sync="value1" />
             <p class="select-content">我要吃：{{ food1 }}</p>
             <p class="select-content">我要玩：{{ sport1 }}</p>
+            <code-example :code="code1" />
         </el-card>
-        <el-card header="使用缓存 - 传入一个由 configItem 组成的数组">
+        <el-card header="使用缓存 - 传入一个由 configItem 组成的数组，可对 title 进行定制">
             <robo-select-multi-group title="又吃又玩" :cache-config="cacheConfig2" :value.sync="value2" />
             <p class="select-content">我要吃：{{ food2 }}</p>
             <p class="select-content">我要玩：{{ sport2 }}</p>
+            <code-example :code="code2" />
         </el-card>
         <el-card header="不使用缓存 - 传入一个由 configItem 组成的数组，并传入具体的 options">
             <robo-select-multi-group title="又吃又玩" :cache-config="cacheConfig3" :value.sync="value3" />
             <p class="select-content">我要吃：{{ food3 }}</p>
             <p class="select-content">我要玩：{{ sport3 }}</p>
+            <code-example :code="code3" />
         </el-card>
     </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue, Prop} from 'vue-property-decorator';
-import RoboSelectMultiComplex from '@/components/robo-select-multi-complex/index.vue';
 import {getFoodToEat} from '@/utils';
 
 @Component
@@ -100,6 +104,50 @@ export default class SelectMultiGroup extends Vue {
     }
     get sport3() {
         return getFoodToEat(this.value3.sport, this.options4);
+    }
+
+    get code1() {
+        return [
+            '<robo-select-multi-group title="又吃又玩" :cache-config="cacheConfig1" :value.sync="value1" />',
+            "cacheConfig1 = ['recipe', 'sport'];",
+            'value1 = {recipe: [], sport: []};'
+        ];
+    }
+
+    get code2() {
+        return [
+            '<robo-select-multi-group title="又吃又玩" :cache-config="cacheConfig2" :value.sync="value2" />',
+            " cacheConfig2 = [{cacheKey: 'recipe', groupTitle: 'Hello'}, {cacheKey: 'sport'}];",
+            ` value2 = {
+    recipe: ['选项1', '选项2', '选项3', '选项4', '选项5'],
+    sport: ['选项1', '选项2']
+};`
+        ];
+    }
+
+    get code3() {
+        return [
+            '<robo-select-multi-group title="又吃又玩" :cache-config="cacheConfig3" :value.sync="value3" />',
+            `cacheConfig3 = [
+    {
+        cacheKey: 'recipe',
+        groupTitle: '嘻嘻嘻',
+        options: [
+            {value: '选项1', label: '啦啦啦'},
+            {value: '选项2', label: '呜呜呜'}
+        ]
+    },
+    {
+        cacheKey: 'sport',
+        groupTitle: '哈哈哈',
+        options: [
+            {value: '选项1', label: '嘎嘎嘎'},
+            {value: '选项2', label: '呼呼呼'}
+
+    }
+];`,
+            'value3 = {recipe: [], sport: []};'
+        ];
     }
 }
 </script>
