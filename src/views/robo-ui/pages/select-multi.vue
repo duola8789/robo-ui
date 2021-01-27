bc44ceacf82bd4910fbd419ec044f888c671309c
zhouhao15 <zhouhao15@baidu.com>
2021-01-27 08:58:18 +0800

    feature: RoboSelectMulti组件更新使用说明

--- src/views/robo-ui/pages/select-multi.vue
+++ src/views/robo-ui/pages/select-multi.vue
@@ -1,20 +1,31 @@
 <template>
     <div class="page-container">
-        <el-card header="基础用法">
+        <el-card header="基础用法：与el-select相比多了前置标题和全选功能">
             <robo-select-multi title="吃货食谱" :options="options" :value.sync="value1" />
             <p class="select-content">我要吃：{{ food1 }}</p>
+            <code-example :code="code1" />
         </el-card>
-        <el-card header="不含标题">
+        <el-card header="改变宽度：传入 width 属性，这样可保证下拉框与输入框宽度一致">
+            <robo-select-multi title="吃货食谱" :options="options" :value.sync="value1" :width="400" />
+            <p class="select-content">我要吃：{{ food1 }}</p>
+            <code-example :code="code5" />
+        </el-card>
+        <el-card header="不含标题：需要显式的传入 hide-title 为 true，配合<form-item>使用时需要传入此属性">
             <robo-select-multi :hide-title="true" :options="options" :value.sync="value2" />
             <p class="select-content">我要吃：{{ food2 }}</p>
+            <code-example :code="code2" />
         </el-card>
         <el-card header="空选项">
             <robo-select-multi :options="[]" :value.sync="value3" />
             <p class="select-content">吃个屁</p>
+            <code-example :code="code3" />
         </el-card>
-        <el-card header="使用缓存（需要后台接口配合统一输出）">
+        <el-card
+            header="使用缓存，需要配合 robo-option-cache 插件使用，传入的 cache-key 应该提前在插件中配置好，配置方法见插件"
+        >
             <robo-select-multi cache-key="recipe" :value.sync="value4" />
             <robo-select-multi cache-key="sport" :value.sync="value5" style="margin-left: 16px;" />
+            <code-example :code="code4" />
         </el-card>
     </div>
 </template>
@@ -47,6 +58,39 @@ export default class SelectMulti extends Vue {
 
     get food2() {
         return this.value2.map((v) => this.options.find((option) => option.value === v)!.label).join(', ');
+    }
+
+    get code1() {
+        return [
+            '<robo-select-multi title="吃货食谱" :options="options" :value.sync="value1" />',
+            'value1 = [];',
+            `options = [
+    {value: '选项1', label: '黄金糕'},
+    {value: '选项2', label: '双皮奶'},
+    {value: '选项3', label: '蚵仔煎'},
+    {value: '选项4', label: '龙须面'},
+    {value: '选项5', label: '北京烤鸭'}
+];`
+        ];
+    }
+
+    get code2() {
+        return '<robo-select-multi :hide-title="true" :options="options" :value.sync="value2" />';
+    }
+
+    get code3() {
+        return '<robo-select-multi :options="[]" :value.sync="value3" />';
+    }
+
+    get code4() {
+        return [
+            '<robo-select-multi cache-key="recipe" :value.sync="value4" />',
+            '<robo-select-multi cache-key="sport" :value.sync="value5" style="margin-left: 16px;" />'
+        ];
+    }
+
+    get code5() {
+        return ['<robo-select-multi title="吃货食谱" :options="options" :value.sync="value1" :width="400" />'];
     }
 }
 </script>
