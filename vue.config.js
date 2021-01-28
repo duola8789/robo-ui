d17c8fdfde6331f9eed8e0e074dc80f25a7fa6f3
zhouhao15 <zhouhao15@baidu.com>
2021-01-28 11:40:03 +0800

    feature: productionSourceMap
    Change-Id: I1fe2dcefe0b053c94708cb950ca85f5b9362b893

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
     devServer: {
         disableHostCheck: true
     },
-    productionSourceMap: false,
+    // productionSourceMap: false,
     configureWebpack: {
         plugins: [
             new StyleLintPlugin({
