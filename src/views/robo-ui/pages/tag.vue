bd84cd0dede5a0e60e9edcd6fdfff1414615e37f
zhouhao15 <zhouhao15@baidu.com>
2021-01-28 10:49:02 +0800

    feature: Fix Tag Code Example
    Change-Id: I4ad32ed4ac9e5e5aaabf4fffb7038b1ad158147e

--- src/views/robo-ui/pages/tag.vue
+++ src/views/robo-ui/pages/tag.vue
@@ -15,15 +15,15 @@
         </el-card>
         <el-card header="自定义颜色：边框颜色与文字颜色一致，只需要为 text-color 传入一个色值，例如 #53A9FF">
             <robo-tag text-color="#53A9FF">黄金糕</robo-tag>
-            <code-example :code="code2" />
+            <code-example :code="code3" />
         </el-card>
         <el-card header="自定义颜色：边框颜色与文字颜色不一致，传入 text-color 之外，再传入 border-color">
             <robo-tag text-color="#53A9FF" border-color="#00CFE6">黄金糕</robo-tag>
-            <code-example :code="code3" />
+            <code-example :code="code4" />
         </el-card>
         <el-card header="自定义颜色：默认不填充背景色，填充背景色时需要传入 background-color">
             <robo-tag text-color="#fff" border-color="#FF6868" background-color="#000">黄金糕</robo-tag>
-            <code-example :code="code4" />
+            <code-example :code="code5" />
         </el-card>
     </div>
 </template>
@@ -49,10 +49,14 @@ export default class Tag extends Vue {
     }
 
     get code3() {
-        return '<robo-tag text-color="#53A9FF" border-color="#00CFE6">黄金糕</robo-tag>';
+        return '<robo-tag text-color="#53A9FF">黄金糕</robo-tag>';
     }
 
     get code4() {
+        return '<robo-tag text-color="#53A9FF" border-color="#00CFE6">黄金糕</robo-tag>';
+    }
+
+    get code5() {
         return ' <robo-tag text-color="#fff" border-color="#FF6868" background-color="#000">黄金糕</robo-tag>';
     }
 }
