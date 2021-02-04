5458dfbda1392a05e9f024b199121c191496ce12
zhouhao15 <zhouhao15@baidu.com>
2021-02-04 14:03:56 +0800

    feature: Robo2ColumnsContainer 导出名称修复
    Change-Id: I5135c5c5726b79c37f4a3d630fe863ea80ff53d5

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const components = [
     {name: 'RoboCheckboxMulti', comp: CheckboxMulti},
     {name: 'RoboCheckboxMultiGroup', comp: CheckboxMultiGroup},
     {name: 'RoboUserAvatar', comp: UserAvatar},
-    {name: 'RoboColumnsContainer', comp: ColumnsContainer},
+    {name: 'Robo2ColumnsContainer', comp: ColumnsContainer},
     {name: 'RoboCheckAll', comp: CheckAll},
     {name: 'RoboInfo', comp: Info},
     {name: 'RoboTag', comp: Tag}
