b57cbe4982a24ecacf26b9a2f07341719d7dd4c4
zhouhao15 <zhouhao15@baidu.com>
2021-02-04 10:16:16 +0800

    feature: Pagination 样式优化
    Change-Id: Ie03e48452453ab658efef6cb88d7d19179c9b4b1

--- src/views/data/pages/pagination.vue
+++ src/views/data/pages/pagination.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="page-container">
         <el-card header="带有背景色的分页：注意不要设置 background 属性">
-            <el-pagination layout="total, prev, pager, next, sizes" :total="1000"></el-pagination>
+            <div class="pagination-container">
+                <el-pagination layout="total, prev, pager, next, sizes" :total="1000"></el-pagination>
+            </div>
             <code-example :code="code1" />
         </el-card>
     </div>
@@ -13,7 +15,15 @@ import {Component, Vue} from 'vue-property-decorator';
 @Component
 export default class Pagination extends Vue {
     get code1() {
-        return '<el-pagination layout="total, prev, pager, next, sizes" :total="1000"></el-pagination>';
+        return '<div><el-pagination layout="total, prev, pager, next, sizes" :total="1000"></el-pagination></div>';
     }
 }
 </script>
+
+<style lang="scss">
+.pagination-container {
+    padding: 16px;
+    border: 1px solid #d1dbe5;
+    overflow: hidden;
+}
+</style>
