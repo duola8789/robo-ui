a02c526df01d79c14120b74dd82aa9fa9d464047
zhouhao15 <zhouhao15@baidu.com>
2021-02-19 11:55:57 +0800

    feature: 优化 el-message-box 样式
    Change-Id: I4a948094de12a05922a8f05f1818db7e8af99907

--- src/views/notice/pages/message-box.vue
+++ src/views/notice/pages/message-box.vue
@@ -2,11 +2,17 @@
     <div class="page-container">
         <el-card class="card-item">
             <p slot="header">确认消息</p>
-            <el-button type="text" @click="open">点击打开 Message Box</el-button>
+            <el-button type="text" @click="open('您确定要退出吗')">点击打开 Message Box</el-button>
+            <el-button type="text" @click="open('您真的真的真的真的真的真的真的确定要退出吗')">
+                点击打开 Message Box
+            </el-button>
         </el-card>
         <el-card class="card-item">
             <p slot="header">居中布局</p>
-            <el-button type="text" @click="open2">点击打开 Message Box</el-button>
+            <el-button type="text" @click="open2('您确定要退出吗')">点击打开 Message Box</el-button>
+            <el-button type="text" @click="open2('您真的真的真的真的真的真的真的确定要退出吗')">
+                点击打开 Message Box
+            </el-button>
         </el-card>
     </div>
 </template>
@@ -16,8 +22,8 @@ import {Component, Vue} from 'vue-property-decorator';
 
 @Component
 export default class ButtonPage extends Vue {
-    open() {
-        this.$confirm('您确定要退出吗?', '提示', {
+    open(msg: string) {
+        this.$confirm(msg, '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
             type: 'warning'
@@ -36,8 +42,8 @@ export default class ButtonPage extends Vue {
             });
     }
 
-    open2() {
-        this.$confirm('您确定要退出登录吗?', '提示', {
+    open2(msg: string) {
+        this.$confirm(msg, '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
             type: 'warning',
