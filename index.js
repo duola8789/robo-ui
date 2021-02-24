d34daf539b8227c6cfc8e1d41c240b63cb985ff6
zhouhao15 <zhouhao15@baidu.com>
2021-02-24 21:50:49 +0800

    feature: 添加 el-step 组件
    Change-Id: I80a760fe3d26bde148b5984d25a475289bd4cebf

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ const components = [
     {name: 'RoboTag', comp: Tag},
     {name: 'RoboFormTitle', comp: FormTitle},
     {name: 'RoboImagePreview', comp: ImagePreview},
-    {name: 'RoboSteps', comp: RoboSteps},
-    {name: 'RoboStep', comp: RoboStep}
+    {name: 'RoboSteps', comp: Steps},
+    {name: 'RoboStep', comp: Step}
 ];
 
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
