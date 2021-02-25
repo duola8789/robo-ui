98d9bfeb9733468d5443b6c5817ef6ef9e265d25
zhouhao15 <zhouhao15@baidu.com>
2021-02-25 09:09:36 +0800

    feature: el-step 组件添加 space 参数
    Change-Id: I9cdd408292bacf40a01974eaf595908409c83d68

--- src/packages/robo-step/index.vue
+++ src/packages/robo-step/index.vue
@@ -21,9 +21,10 @@ export default class RoboStep extends Vue {
 
     get space() {
         const space = (this.$parent as any).space;
+        debugger;
         if (space) {
             return {
-                height: `${space}px`
+                height: `${space + 28}px`
             };
         }
         return {height: '100%'};
@@ -74,6 +75,8 @@ $finish-color: #055fe7;
 
     .robo-step-right {
         .robo-step-title {
+            height: 20px;
+            line-height: 20px;
             color: #181c32;
             margin-bottom: 8px;
         }
